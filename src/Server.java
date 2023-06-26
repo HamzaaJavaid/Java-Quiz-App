aaeec639452c7ea6e094c3a0e56bb089fdb25f9e
Hamza Javaid <115164085+HamzaaJavaid@users.noreply.github.com>
2023-06-26 18:09:57 +0500

    Update Server.java
    Quiz now include Mcqs Format . 
    It also show the correct andswer and user entered answer as well

--- src/Server.java
+++ src/Server.java
@@ -1,10 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class Server {
@@ -13,7 +10,7 @@ public class Server {
     private static final String PASSWORD = "0767";
     private static final int QUESTION_TIMEOUT_SECONDS = 15;
 
-    private static final List<String> generalKnowledgeQuestions = new ArrayList<>();
+    private static final List<Question> generalKnowledgeQuestions = new ArrayList<>();
 
     public static void main(String[] args) {
         initializeGeneralKnowledgeQuestions();
@@ -52,18 +49,31 @@ public class Server {
             // Start the quiz
             Quiz quiz = new Quiz();
             for (int i = 0; i < generalKnowledgeQuestions.size(); i++) {
-                String question = generalKnowledgeQuestions.get(i);
-                writer.println("Question " + (i + 1) + ": " + question);
+                Question question = generalKnowledgeQuestions.get(i);
+                writer.println("Question " + (i + 1) + ": " + question.getQuestionText());
+                writer.println("Options:");
 
-                QuizTimerTask timerTask = new QuizTimerTask(reader, quiz, question);
+                List<String> options = question.getOptions();
+                for (int j = 0; j < options.size(); j++) {
+                    writer.println((char) ('A' + j) + ") " + options.get(j));
+                }
+
+                QuizTimerTask timerTask = new QuizTimerTask(reader, writer, quiz, question);
                 Timer timer = new Timer();
-                timer.schedule(timerTask, TimeUnit.SECONDS.toMillis(QUESTION_TIMEOUT_SECONDS));
+                timer.schedule(timerTask, QUESTION_TIMEOUT_SECONDS * 1000);
 
                 String answer = timerTask.waitForAnswer();
 
                 timer.cancel();
 
-                quiz.addQuestionAnswer(question, answer);
+                quiz.addAnswer(question, answer);
+
+                String correctAnswer = question.getCorrectAnswer();
+                if (answer.equalsIgnoreCase(correctAnswer)) {
+                    writer.println("Your answer is correct!");
+                } else {
+                    writer.println("Your answer is incorrect. The correct answer is: " + correctAnswer);
+                }
             }
 
             // Store the quiz in a separate text file
@@ -99,21 +109,94 @@ public class Server {
     }
 
     private static void initializeGeneralKnowledgeQuestions() {
-        generalKnowledgeQuestions.add("Who invented the telephone?");
-        generalKnowledgeQuestions.add("What is the capital of France?");
-        generalKnowledgeQuestions.add("Who painted the Mona Lisa?");
-        generalKnowledgeQuestions.add("What is the largest planet in our solar system?");
-        generalKnowledgeQuestions.add("What year was the first manned moon landing?");
+        Question question1 = new Question(
+                "Which planet is known as the Red Planet?",
+                Arrays.asList("A) Venus", "B) Mars", "C) Jupiter", "D) Saturn"),
+                "B"
+        );
+        Question question2 = new Question(
+                "What is the capital of France?",
+                Arrays.asList("A) London", "B) Paris", "C) Rome", "D) Madrid"),
+                "B"
+        );
+        Question question3 = new Question(
+                "Who painted the Mona Lisa?",
+                Arrays.asList("A) Leonardo da Vinci", "B) Vincent van Gogh", "C) Pablo Picasso", "D) Michelangelo"),
+                "A"
+        );
+        Question question4 = new Question(
+                "Which country won the FIFA World Cup in 2018?",
+                Arrays.asList("A) Germany", "B) Brazil", "C) France", "D) Argentina"),
+                "C"
+        );
+        Question question5 = new Question(
+                "What is the largest ocean on Earth?",
+                Arrays.asList("A) Atlantic Ocean", "B) Indian Ocean", "C) Arctic Ocean", "D) Pacific Ocean"),
+                "D"
+        );
+
+        generalKnowledgeQuestions.add(question1);
+        generalKnowledgeQuestions.add(question2);
+        generalKnowledgeQuestions.add(question3);
+        generalKnowledgeQuestions.add(question4);
+        generalKnowledgeQuestions.add(question5);
+    }
+
+    private static class Question {
+        private final String questionText;
+        private final List<String> options;
+        private final String correctAnswer;
+
+        public Question(String questionText, List<String> options, String correctAnswer) {
+            this.questionText = questionText;
+            this.options = options;
+            this.correctAnswer = correctAnswer;
+        }
+
+        public String getQuestionText() {
+            return questionText;
+        }
+
+        public List<String> getOptions() {
+            return options;
+        }
+
+        public String getCorrectAnswer() {
+            return correctAnswer;
+        }
+    }
+
+    private static class Quiz {
+        private final Map<Question, String> questionAnswers;
+
+        public Quiz() {
+            questionAnswers = new HashMap<>();
+        }
+
+        public void addAnswer(Question question, String answer) {
+            questionAnswers.put(question, answer);
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            for (Map.Entry<Question, String> entry : questionAnswers.entrySet()) {
+                sb.append(entry.getKey().getQuestionText()).append("\n");
+                sb.append("Answer: ").append(entry.getValue()).append("\n\n");
+            }
+            return sb.toString();
+        }
     }
 
     private static class QuizTimerTask extends TimerTask {
         private final BufferedReader reader;
+        private final PrintWriter writer;
         private final Quiz quiz;
-        private final String question;
-        private String answer;
+        private final Question question;
 
-        public QuizTimerTask(BufferedReader reader, Quiz quiz, String question) {
+        public QuizTimerTask(BufferedReader reader, PrintWriter writer, Quiz quiz, Question question) {
             this.reader = reader;
+            this.writer = writer;
             this.quiz = quiz;
             this.question = question;
         }
@@ -121,10 +204,10 @@ public class Server {
         @Override
         public void run() {
             try {
-                if (answer == null) {
-                    answer = ""; // Set blank answer if no response received within the timeout
+                if (!reader.ready()) {
+                    writer.println("Time's up! Moving to the next question.");
                 }
-            } catch (Exception e) {
+            } catch (IOException e) {
                 e.printStackTrace();
             }
         }
@@ -134,7 +217,9 @@ public class Server {
                 long startTime = System.currentTimeMillis();
                 while ((System.currentTimeMillis() - startTime) < TimeUnit.SECONDS.toMillis(QUESTION_TIMEOUT_SECONDS)) {
                     if (reader.ready()) {
-                        answer = reader.readLine();
+                        String answer = reader.readLine().trim().toUpperCase();
+                        writer.println("Your answer: " + answer);
+
                         return answer;
                     }
                     TimeUnit.MILLISECONDS.sleep(100);
@@ -146,6 +231,3 @@ public class Server {
         }
     }
 }
-
-
-
